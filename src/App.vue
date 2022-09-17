5a392e5ea377199e5ce3e5dfbdedb58f15f52a97
Luan Almeida <luanbianchine1@gmail.com>
2022-09-17 14:24:30 -0300

    search system concluded

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,15 @@
 <template>
 <div id="app">
   <div class="column is-half is-offset-one-quarter">
-    <div v-for="(poke, index) in pokemons" :key="index">
-      <PokemonVue :name="poke.name" :url="poke.url" :num="index"/>
+    <input 
+      type="text" 
+      placeholder="Buscar pokemon" 
+      v-model="busca"
+      class="input is-rounded"
+    />
+    <button class="button is-fullwidth is-success" id="buscaBtn" @click="buscar">Buscar</button>
+    <div v-for="poke in filteredPokemons" :key="poke.url">
+      <PokemonVue :name="poke.name" :url="poke.url"/>
     </div>
   </div>
 </div>
@@ -16,16 +23,35 @@ export default {
   name: 'App',
   data(){
     return{
-      pokemons: []
+      pokemons: [],
+      filteredPokemons: [],
+      busca: ''
     }
   },
   created : function(){
     axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0").then(res => {
       this.pokemons = res.data.results
+      this.filteredPokemons = res.data.results
     })
   },
   components:{
     PokemonVue
+  },
+  methods: {
+    buscar: function(){
+      this.filteredPokemons = this.pokemons
+      if(this.busca == ''){
+        this.filteredPokemons = this.pokemons
+      } else {
+        this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.busca)
+      }
+    }
   }
 }
 </script>
+
+<style>
+  #buscaBtn{
+    margin-top: 2%;
+  }
+</style>
--- src/components/PokemonVue.vue
+++ src/components/PokemonVue.vue
@@ -45,7 +45,6 @@ export default {
     }
   },
   props: {
-    num: Number,
     name: String,
     url: String,
   },
