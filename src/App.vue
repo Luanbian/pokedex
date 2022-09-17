7bcbf76f542c7a63257e17d0fc545c1e9d14d8b8
Luan Almeida <luanbianchine1@gmail.com>
2022-09-17 13:12:46 -0300

    add pokemon component

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <template>
-<div>
-  
+<div id="app">
+  <div class="column is-half is-offset-one-quarter">
+    <div v-for="(poke, index) in pokemons" :key="index">
+      <PokemonVue :name="poke.name" :url="poke.url" :num="index"/>
+    </div>
+  </div>
 </div>
 </template>
 
 <script>
+import axios from 'axios'
+import PokemonVue from './components/PokemonVue'
 
 export default {
-  name: 'App'
+  name: 'App',
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created : function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=152&offset=0").then(res => {
+      this.pokemons = res.data.results
+    })
+  },
+  components:{
+    PokemonVue
+  }
 }
 </script>
